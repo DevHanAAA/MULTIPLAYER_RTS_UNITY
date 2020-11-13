1c554766df4dd67704d6ac7622bd06e6c078e067
Dev.Han <developerhan89@gmail.com>
2020-11-12 21:49:25 -0600

    Spawning Units

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null; //Reference to thing to spawn
+    [SerializeField] private Transform unitSpawnPoint = null; // under UnitSpawner
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        // Spawn by Server
+        GameObject unitInstance = Instantiate(
+            unitPrefab,
+            unitSpawnPoint.position,
+            unitSpawnPoint.rotation);
+
+        NetworkServer.Spawn(unitInstance, connectionToClient); // Spawn by Client & Give Ownership
+    }
+
+    #endregion
+
+    #region Client
+
+    // Unity will Call this Event Function whenever client clicks Game Object
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button != PointerEventData.InputButton.Left) { return; }
+
+        if (!hasAuthority) { return; }
+
+        CmdSpawnUnit(); // Call Command that's originally in server code
+    }
+
+    #endregion
+}
+
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,17 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
-    
+    [SerializeField] private GameObject unitSpawnerPrefab = null;
+
+    public override void OnServerAddPlayer(NetworkConnection conn) // Add player by server
+    {
+        base.OnServerAddPlayer(conn);
+        //Spawn base on server
+        GameObject unitSpawnerInstance = Instantiate( 
+            unitSpawnerPrefab, 
+            conn.identity.transform.position, 
+            conn.identity.transform.rotation);
+
+        NetworkServer.Spawn(unitSpawnerInstance, conn); // Spawn on client & Give ownership
+    }
 }
